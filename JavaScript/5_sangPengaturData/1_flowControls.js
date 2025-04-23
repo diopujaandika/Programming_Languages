7002b091c7c517295a37a1890ce2808797de7cc6
diopujaandika <dpujaandika17@gmail.com>
2025-04-23 19:22:35 +0700

    try

--- JavaScript/5_sangPengaturData/1_flowControls.js
+++ JavaScript/5_sangPengaturData/1_flowControls.js
@@ -1,5 +1,10 @@
 /**
- * Flow Controls
+ * Flow Controls adalah urutan bagaimana expression dan statement dijalankan oleh komputer berdasarkan kondisi dan logikan tertentu.
+ * 
+ * Jenis Flow Control:
+ * 1. Conditional
+ * 2. Looping
+ * 3. Error Handling
  */
 
 const number = 100
