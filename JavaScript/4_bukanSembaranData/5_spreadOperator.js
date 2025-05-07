15128beeca5c703cc3b63a4c3eb6f2f6ba9da8e1
diopujaandika <dpujaandika17@gmail.com>
2025-05-07 13:50:01 +0700

    belajar spread operator

--- JavaScript/4_bukanSembaranData/5_spreadOperator.js
+++ JavaScript/4_bukanSembaranData/5_spreadOperator.js
@@ -1,24 +1,24 @@
+//SPREAD OPERATOR
 /**
- * Spread Operator
- * Spread Operator digunakan untuk menyebarkan nilai yang ada pada object dan array dengan sintak ...namaObject
+ * Spread operator digunakan untuk menyebarkan nilai yang ada pada object dan array. Spread opertor ditantai dengan tiga titik (...) sehingga nilai object dan array dapat di-iterable menjadi beberapa element.
  */
 
-//Contoh dalam Object
-    const obj1 = {name: 'Dicoding'}
-    const obj2 = {lastNama: 'Indonsia', adress: 'Jl. Batik Kumeli No. 50'}
-    //Memudahkan untuk mengabungkan object
-    const newObj = {...obj1, ...obj2}
-    console.log(newObj)
-    //Memudahkan menyalin object
-    const copieObj = {...obj2}
-    console.log(copieObj)
+//OBJECT
+    const objectSatu = {name: 'Dicoding'}
+    const objectDua = {lastName: 'Indonesia', address: 'Jln. Batik Kumeli No. 50', years: 10}
+    //Mempermudah mengabungkan dua atau lebih object
+    const objectMenyebar = {...objectSatu, ...objectDua}
+    console.log(objectMenyebar)
+    //Mempermudah menyalin suatu object
+    const objectMenyalin = {...objectMenyebar}
+    console.log(objectMenyalin)
 
-//Contoh dalam Array
-    const array1 = ["Dicoding"]
-    const array2 = ["Indonesia", "Jl. Batik Kumeli No. 50"]
-    //Memudahkan untuk mengabungkan Array
-    const newArray = [...array1, ...array2]
-    console.log(newArray)
-    //Memudahkan untuk menyalin Array
-    const copieArray = [...array2]
-    console.log(copieArray)
+//ARRAY
+    const arraySatu = ['Dicoding']
+    const arrayDua = ['Indonesia', 'Jln. Batik Kumeli No. 50', 11]
+    //Mempermudah mengabungkan dua atau lebih array
+    const arrayMenyebar = [...arraySatu, ...arrayDua]
+    console.log(arrayMenyebar)
+    //Mempermudah menyalin suatu array
+    const arrayMenyalin = [...arrayMenyebar]
+    console.log(arrayMenyalin)
