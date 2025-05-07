d1451d2afe0ede1bf3a79b4264a6b91c6bda6cfb
diopujaandika <dpujaandika17@gmail.com>
2025-05-07 14:29:31 +0700

    rest opertor'

---
+++ JavaScript/4_bukanSembaranData/0_strucktur_data.js
@@ -0,0 +1,10 @@
+//STRUKTUR DATA 
+/**
+ * Untuk memudahkan seorang programmer dalam menyelesaikan masalah secara efektif terdapat istilah yang sering dikenal dengan struktur data.
+ * Pada JavaSciprt terdapat beberapa jenis struktur data:
+ * 1. Object {}
+ * 2. Array []
+ * 3. Map
+ * 4. Set
+ * 5. Spread Operator (...object/...array)
+ */
--- JavaScript/4_bukanSembaranData/6_restOperator.js
+++ JavaScript/4_bukanSembaranData/6_restOperator.js
@@ -1,9 +1,31 @@
+//REST OPERATOR
 /**
- * Rest Operator memungkinkan function untuk menerima argument dalam bentu array 
+ * Rest Opertor digunakan function untuk menerima argument dalam bentuk array. Rest operator yang digunakan pada parameter function disebut dengan Rest Parameter (...)
  */
 
-function myFunction(...manyMoreArgs){
-    console.log('manyMoreArgs', manyMoreArgs)
-}
+    function namaFunction(...parameter){
+        console.log('nama: ', parameter)
+    }
+    namaFunction('Tony Stark', 'Stave Rogers', 'Thor')
 
-myFunction('One Be', 1.2, false)
+    //Bagaimana dengan dua atau lebih dari satu parameter?
+    function avengers(vilain, ...hero){
+        console.log('Baiik: ', hero)
+        console.log('Jahat: ', vilain)
+    }
+    avengers('Dr. Doom', 'Iron Man', 'Captain Amerika', 'Thor')
+
+    //Untuk melihat jumlah data yang ada menggunakan methode => namaArray.length
+    function namaAntiHero(...antiHero){
+        console.log(antiHero.length)
+        console.log('Nama : ', antiHero)
+    }
+    namaAntiHero('Venom', 'Deadpool', 'Loki', 'Moon Knight')
+
+    //Untuk menangkap sisa elemen yang dipanggil sebelumnya
+    const hero = ['Iron Man', 'Captain America', 'Thor', 'Hulk', 'Black Widow', 'Hawkeye']
+    const [first, second, ...rest] = hero
+
+    console.log(first)
+    console.log(second)
+    console.log(rest)
