31ebb88808f4508454e3a191aab6cc6d628180ba
diopujaandika <dpujaandika17@gmail.com>
2025-05-21 08:35:27 +0700

    module export

---
+++ JavaScript/6_modularisasi_harmonis/3_module/1_module_export.mjs
@@ -0,0 +1,23 @@
+//MODULE
+/**
+ * 
+ */
+
+//Cara Export Variable
+    export const name = 'Dicoding'
+    export const favoriteFood = ['Pizza', 'Pasta', 'Shushi']
+
+//Cara Export Function
+    export function sayHi(name){
+        console.log(`Hi, ${name}`)
+    }
+
+//Sebenarnya tidak perlu menuliskan kata kunci export disetiap nilai yang ingin dieksport. Kita cukup mengeksport diakhir berikas:
+    const nama = 'Dicoding'
+    const makananKesukaan = ['Mi Ayam', 'Sate', 'Nasi Goreng', 'Bakso']
+
+    function iniSaya(nama, makananKesukaan){
+        console.log(`Hai, ini ${nama}, makanan kesukaan saya adalah ${makananKesukaan}`)
+    }
+
+    export{nama, makananKesukaan, iniSaya}
---
+++ JavaScript/6_modularisasi_harmonis/3_module/2_module_import.js
@@ -0,0 +1,6 @@
+import {name, favoriteFood, sayHi, nama, makananKesukaan, iniSaya} from './1_module_export.mjs'
+
+console.log(name)
+console.log(favoriteFood)
+sayHi('Dio')
+iniSaya('Dudi', 'Sate')
