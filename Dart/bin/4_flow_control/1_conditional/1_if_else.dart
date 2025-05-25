09256df136ce8119f3155557352d98aa42868ec0
diopujaandika <dpujaandika17@gmail.com>
2025-05-26 00:30:37 +0700

    flow control1

---
+++ Dart/bin/4_flow_control/1_conditional/1_if_else.dart
@@ -0,0 +1,15 @@
+///IF AND ELSE
+
+void main(){
+  controlIf();
+}
+
+void controlIf(){
+  var isRaining = true;
+  print("Prepare before going to offiece?");
+
+  if(isRaining){
+    print("Oh... It's raining, bring an umbrella.");
+  }
+  print("Going to the office!");
+}
