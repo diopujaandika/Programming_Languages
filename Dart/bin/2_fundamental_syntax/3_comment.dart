b9c35c694d8a891377ab5d9fa3d452c9beb6f99a
diopujaandika <dpujaandika17@gmail.com>
2025-06-21 11:55:12 +0700

    comment in dart

--- Dart/bin/2_fundamental_syntax/3_comment.dart
+++ Dart/bin/2_fundamental_syntax/3_comment.dart
@@ -1,15 +1,26 @@
 ///COMMNET
-///Comment adalah instruksi yang dibuat tetapi tidak akan dieskesusi oleh program.
-///Comments dalam Dart
 ///1. Single line comment => //
 ///2. Multi line comment => /**/
-///3. Documentation comment ///
+///3. Documentation comment => ///
 
-void main(){
-  //Single line comment
-  print("Single line comment!");
+///Fungsi [main] akan menampilkan 2 Output
+///Output pertama menampilkan teks dan output kedua menampilkan hasil perkalian pada fungsi [calculate]
+
+void main(List<String> arguments){
+  //Mencetak Hello Dart! Dart is great in konsole.
+  print("Hello Dart! Dart is that the Single line Comment.");
   /*
    * Multi line commnet
    */
-  print("Multi line comment!");
+  print("Multi line Comment! 6 * 7 = ${calculate()}");
 }
+
+int calculate(){
+  return 6*7;
+}
+
+/**
+ * Output:
+ * Hello Dart! Dart is that the Single line Comment.
+ * Mutli line Comment! 6 * 7 = 42
+ */
