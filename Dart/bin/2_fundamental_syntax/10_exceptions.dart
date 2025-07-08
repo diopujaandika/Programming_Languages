2e16aebaa978d2fd3ec271582360f443523b6d77
diopujaandika <dpujaandika17@gmail.com>
2025-07-08 11:07:01 +0700

    update dart fundamental -exceptions

--- Dart/bin/2_fundamental_syntax/10_exceptions.dart
+++ Dart/bin/2_fundamental_syntax/10_exceptions.dart
@@ -1,14 +1,21 @@
 ///EXCEPTIONS
+  ///Exceptions adalah keadaan saat aplikasi di runtime terjadi error atau crash.
+  ///Ketika exception terjadi, aplikasi akan dihentikan dan kode program setelahnya tidak akan dieksekusi.
+  ///Exceptions memiliki setidaknya 3 block dasar yaitu:
+    ///1. try => untuk menampung kode-kode yang kemungkinan terjadi error atau crash
+    ///2. on Exception => untuk mengatasi exception yang diketahui secara spesifik
+    ///3. catch => untuk mengatasi exception yang tidak diketahui secara spesifik
+    ///4. final => untuk tetap menjalankan kode tanpa peduli hasi dari yang teradi pada blok try-catch
 
 void main(){
-  try{
+  try{  //block try digunakan sebagai block kode yang kemungkinan terjadi erro/crash
     var a = 7;
     var b = 0;
     print(a ~/ b);
-  } catch (e, s){
+  } catch (e, s){ //block mengatasi exceptions 
     print('Exception happened: $e');
     print('Stack trace: $s');
-  }finally{
+  }finally{ //block yang akan terus dijalan walaupun terdapat excepstion
     print("finally tetap akan dijalankan tanpa memperdulikan try dan catch.");
     print("this line still executerd!");
   }
