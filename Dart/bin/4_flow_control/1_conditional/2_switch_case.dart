857f403f71b04951338caf0e36ce48aea1d75768
diopujaandika <dpujaandika17@gmail.com>
2025-07-08 17:15:36 +0700

    update dart fundamental -conditional | switch and case

--- Dart/bin/4_flow_control/1_conditional/2_switch_case.dart
+++ Dart/bin/4_flow_control/1_conditional/2_switch_case.dart
@@ -1,4 +1,21 @@
-///SWITCH CASE
+///SWITCH and CASE
+///Switch and Case adalah pengkondisian yang digunakan untuk mengecek suatu kondisi dengan skala yang lebih besar
+
+///Sintak
+///   switch (variable/expression){
+///   case value1:
+///       statement;
+///       break;
+///   case value2:
+///       statement;
+///       break;
+///   case value3:
+///       statement;
+///       break;
+///   case ...
+///   default:
+///       statement;
+///   }
 
 void main(){
   mySwitch(10, 5, '+');
