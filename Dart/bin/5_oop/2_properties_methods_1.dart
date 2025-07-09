455866dd7a6445cb982c3756723526ada56b7a13
diopujaandika <dpujaandika17@gmail.com>
2025-07-09 08:46:08 +0700

    oop -properties & method | method

--- Dart/bin/5_oop/2_properties_methods_1.dart
+++ Dart/bin/5_oop/2_properties_methods_1.dart
@@ -1,11 +1,11 @@
 ///PROPERTIES & METHODES
+///Method adalah function yang berada di dalam class
 
 import '2_properties_methods_2.dart';
 
 void main(){
   var dicodingCat = Animal('Gray', 2, 3.10)
     ..sleep();
-  ;
 
   dicodingCat.eat();
   dicodingCat.poop();
---
+++ Dart/bin/5_oop/2_properties_methods_4.dart
@@ -0,0 +1,33 @@
+void main(){
+  var person1 = Person();
+
+  person1.name = "Dio Puja Andika";
+  person1.address = "Bengkulu";
+  
+  print(person1.name);
+  print(person1.address);
+  print(person1.countrty);
+  person1.sayHallo("Budi Haryanto");
+  person1.hello();
+  person1.getName();
+}
+
+class Person {
+  //Field || Properties
+  String name = "Guest";
+  String? address;
+  final String countrty = "Indonesia";
+
+  //Method
+  void sayHallo(String paramName){
+    print('Hallo $paramName, My Name is $name');
+  }
+
+  void hello(){
+    print('Hello, my name is $name');
+  }
+
+  String getName(){
+    return "Heloo, my name is $name";
+  }
+}
