d8de189e894d7465ccd03200246846e546026ea7
diopujaandika <dpujaandika17@gmail.com>
2025-09-27 05:42:23 +0700

    update

--- Dart/bin/3_function/1_function.dart
+++ Dart/bin/3_function/1_function.dart
@@ -5,7 +5,7 @@
 ///   returnType functionName(type param1, type param2, ...){
 ///         return result;
 ///   }
-/// 
+///
 
 ///Jenis Parameter dalam Dart
 ///1. origin parameter => type param1, type param2, ...
@@ -15,38 +15,55 @@
 ///5. required parameter => {required type param1, required type param2, ...}
 
 void main(){
-  greet('Dio Puja Andika', 'Dart'); //memanggil function greet
+  greet1('Dio Puja Andika', 'Dart'); //memanggil function greet
+  greet2('Diva Puja Kiranzi', 'Dart');
 
   var firstNum = 7;
   var secondNum = 10;
-  print('Rata-rata $firstNum & $secondNum adalah ${average(firstNum, secondNum)}');
+  print('Rata-rata $firstNum & $secondNum adalah ${average1(firstNum, secondNum)}');
 
-  average(2, 3);   //function mengembalikan nilai
+  average1(2, 3);   //function mengembalikan nilai
+  average2(5, 10);
 
   print(penjumlahan(20, 30));
+  print(pengurangan(30, 10));
 
   greetNewUser1(); //function value default
+  greetNewUser1a();
   greetNewUser2(age: 10, name: 'Wildan', isVerified: false);
   greetNewUser3(name: 'Dio', age: 22, isVerified: true);
 }
 
 //Function yang tidak menghasilkan output atau tidak mengembalikan nilai adalah void
-void greet(String param1, String param2){
+void greet1(String param1, String param2){
   print('Halo $param1, selamat datang di $param2!');
+}
+void greet2(String param1, String param2){
+  print('Hello $param1, selamat datang di $param2!');
 }
 
 //Function yang mengembalikan nilai (return)
-double average(num num1, num num2){
+double average1(num num1, num num2){
   var result = (num1 + num2)/2;
   print('hasil dari ($num1 + $num2) / 2 = $result');
   return result;
 }
-//Arrow Function =>
+double average2(num num1, num num2){
+  var result = (num1 + num2)/2;
+  print('Hasil dari $num1 + $num2 /2 adalah $result');
+  return result;
+}
+
+//Arrow Function
 double penjumlahan(double angka1, double angka2) => (angka1 + angka2);
+double pengurangan(double angka1, double angka2) => (angka1 - angka2);
 
 //Optional Parameters (default value)
 void greetNewUser1([String name = 'dune', int age=10, bool isVerified=true]){
   print('my name is $name, my current age  is $age years old. $isVerified');
+}
+void greetNewUser1a([String name= 'User', int age = 23, bool isVerified = true]){
+  print('Hello $name$age, status Anda $isVerified');
 }
 
 //Optional Parameters dengan named optional parameters (?) memungkinkan memasukkan parameter tanpa mempedulikan urutan parameternnya dengan hanya menyebut nama parameternya.
--- Dart/bin/6_functional_programming/1_fp.dart
+++ Dart/bin/6_functional_programming/1_fp.dart
@@ -1,23 +1,19 @@
 void main(){
-  //Pure Function
-  int sum(int num1, int num2){
-    return num1 + num2;
+  sum(3, 6);
+}
+
+//1. Pure Functions => Function bergantung dengan argurment atau parameter 
+int sum(int num1, int num2){
+  return num1 + num2;
+}
+
+//2. Recursion => FP tidak ada konsep perulangan seperti for atau while
+int fibonacci(n){
+  if(n <= 0){
+    return 0;
+  }else if(n == 1){
+    return 1;
+  }else {
+    return fibonacci(n-1) + fibonacci(n-2);
   }
-
-  //Recursion
-  int fibonacci(n){
-    if(n<=0){
-      return 0;
-    }else if(n==1){
-      return 1;
-    }else{
-      return fibonacci(n-1)+fibonacci(n-2);
-    }
-  }
-
-  //Immutable Variables
-  var x = 5;
-  x = x+1;
-
-  //Function are first-class citizen and can be higher-order
 }
