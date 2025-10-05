2fa872d77cd50e78d6000e69c2b37792d2f0bd47
diopujaandika <dpujaandika17@gmail.com>
2025-10-05 13:04:58 +0700

    update python

--- python/data_interaction/2_data_type.py
+++ python/data_interaction/2_data_type.py
@@ -100,3 +100,124 @@ print("Nama Saya {}".format(name))
 Output: 
 Nama saya Perseus Evans
 """
+
+#List
+x = [1, 2.2, 'Dicoding', True]
+print(x)
+print(type(x))
+print(x[2])
+x[2] = 'Indonesia'
+print(x[2])
+
+x = ["laptop", "monitor", "mouse", "mousepad", "keyboard", "webcam", "microphone"]
+print(x[0])
+print(x[2])
+print(x[-1])
+print(x[-3])
+"""
+Output:
+laptop
+mouse
+microphone
+keyboard
+"""
+
+x = ["laptop", "monitor", "mouse", "mousepad", "keyboard", "webcam", "microphone"]
+
+print(x[0:5:2])
+print(x[1:])
+print(x[:3])
+
+"""
+Output:
+['laptop', 'mouse', 'keyboard']
+['monitor', 'mouse', 'mousepad', 'keyboard', 'webcam', 'microphone']
+['laptop', 'monitor', 'mouse']
+"""
+
+#TUPLE
+x = (1, "Dicoding", 1+3j)
+print(x)
+print(type(x))
+
+"""
+Output:
+<class 'tuple'>
+"""
+
+x = (5, 'program', 1+3j)
+print(x[1])
+print(x[0:3])
+
+""" 
+Output:
+program
+(5, 'program', (1+3j))
+"""
+
+#SET
+x = {1, 2, 7, 2, 3, 13, 3}
+print(x)
+print(type(x))
+
+"""
+Output:
+{1, 2, 3, 7, 13}
+<class 'set'>
+"""
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+union = set1.union(set2)
+print("Union:", union)
+
+intersection = set1.intersection(set2)
+print("Intersection:", intersection)
+
+"""
+Output:
+Union: {1, 2, 3, 4, 5, 6, 7, 8}
+Intersection: {4, 5}
+
+"""
+
+#DICTIONARY
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+print(x['name'])
+print(type(x))
+
+"""
+Output:
+Perseus Evans
+<class 'dict'>
+"""
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+x ['Job'] = "Web Developer"
+
+print(x)
+
+"""
+Output:
+{'name': 'Perseus Evans', 'age': 20, 'isMarried': False, 'Job': 'Web Developer'}
+"""
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+del x['isMarried']
+
+print(x)
+
+"""
+Output:
+{'name': 'Perseus Evans', 'age': 20}
+"""
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+x ['name'] = "Dicoding"
+
+print(x)
+
+"""
+Output:
+{'name': 'Dicoding', 'age': 20, 'isMarried': False}
+"""
