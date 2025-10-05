211a35e371a5935639072c1c627617dde13ad315
diopujaandika <dpujaandika17@gmail.com>
2025-10-05 14:37:48 +0700

    learn make convertion data in python

---
+++ python/data_interaction/3_convertion_data_type.py
@@ -0,0 +1,51 @@
+#konversi integer ke float
+print(float(5))
+"""
+Output:
+5.0
+"""
+
+#konversi float ke integer
+print(int(5.6))
+print(int(-5.6)) 
+""" 
+Output:
+5
+-5
+"""
+
+#konversi dari-dan-ke String
+print(int("25"))
+print(str(25))
+print(float("25"))
+print(str(25.6))
+"""
+Output:
+25
+25
+25.0
+25.6
+"""
+
+#konversi kumpulan data
+print(set([1,2,3]))
+print(tuple({5,6,7}))
+print(list('hello'))
+"""
+Output:
+{1,2,3}
+(5,6,7)
+['h','e','l','l','o']
+"""
+
+#konversi ke dictionary
+print(dict([[1,2],[3,4]]))
+"""
+Output:
+{1:2, 3:4}
+"""
+print(dict([(3,26),(4,44)]))
+"""
+Output:
+{3:26, 4:44}
+"""
