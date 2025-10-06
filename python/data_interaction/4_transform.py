5c98f3eb75d8c7c4bd1181d07b5e996c31c23e57
diopujaandika <dpujaandika17@gmail.com>
2025-10-06 22:00:19 +0700

    Memisahkan dan manggabungkan String

--- python/data_interaction/4_transform.py
+++ python/data_interaction/4_transform.py
@@ -13,3 +13,15 @@ kata = 'CodeCodeDicodingCodeCode'
 print(kata.strip("Code"))
 print('Dicoding Indonesia'.startswith('Dicoding'))
 print('Dicoding Indonesia'.endswith('Dicoding'))
+
+#Memeisahkan dan menggabungkan String
+print(' ' .join(['Dicoding', 'Indonesia', '!']))
+print('123'.join(['Dicoding','Indoenesia']))
+print('Dicoding Indoenesia!'.split())
+print('''Halo,
+aku ikan,
+aku suka sekali menyelam
+aku tinggal di perairan.
+Badanku licin dan renangku cepat.
+Senang berkenalan denganmu.'''.split('\n'))
+
