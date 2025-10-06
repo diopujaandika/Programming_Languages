b93e69e308511d3bc2a0bc48ceac3aed7d1aa5f4
diopujaandika <dpujaandika17@gmail.com>
2025-10-06 23:40:30 +0700

    make formatting at string in python

--- python/data_interaction/4_transform.py
+++ python/data_interaction/4_transform.py
@@ -39,3 +39,12 @@ print(kata.isalpha())
 print('123'.isdecimal())
 print('             '.isspace())
 print('Dicoding Indonesia'.istitle())
+
+#Formatting pada String
+teks = 'Code'
+tambah_number = teks.zfill(5)
+print(tambah_number)
+print('Dicoding'.rjust(20))
+print('Dicoding'.rjust(20, '!'))
+print('Dicoding'.ljust(20))
+print('Dicoding'.center(10, '-'))
