dd2505a9344307c12202e014d9d196a4ae0ade88
diopujaandika <dpujaandika17@gmail.com>
2025-10-09 23:46:41 +0700

    kotlin?

--- python/data_interaction/5_operation_list_set_string.py
+++ python/data_interaction/5_operation_list_set_string.py
@@ -2,3 +2,15 @@
 contoh_list = [1, 3, 3, 5, 5, 5, 7, 7, 9]
 print(contoh_list)
 print('panjang list adalah', len(contoh_list))
+
+#SET
+contoh_list = set([1, 3, 3, 5, 5, 5, 7, 7, 9])
+print(contoh_list)
+print('panjang list adalah', len(contoh_list))
+
+#STRING
+contoh_list = "Belajar Python"
+print(contoh_list)
+print('panjang list adalah', len(contoh_list))
+
+#min() dan max()
