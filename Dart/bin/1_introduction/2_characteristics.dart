0dfbb03325a9beb1e15d70987d21f3f1191ceb38
diopujaandika <dpujaandika17@gmail.com>
2025-10-15 12:44:57 +0700

    learn about a characteristics in dart

--- Dart/bin/1_introduction/2_characteristics.dart
+++ Dart/bin/1_introduction/2_characteristics.dart
@@ -4,22 +4,22 @@ void main(){
   print('Hello $name, Welcome to $language');
 
   //Memanggil Function
-  StaticallyType('Dio Puja Andika', 23);
-  TypeInference('Dio Puja Andika', 23);
-  StringInterpolation('Dio Puja Andika', 23);
+  staticallyType('Dio Puja Andika', 23);
+  typeInference('Dio Puja Andika', 23);
+  stringInterpolation('Dio Puja Andika', 23);
 }
 
 //KARAKTERISTIK DART
 //1. Statically Typed
-void StaticallyType(String name, int age){
+void staticallyType(String name, int age){
   print('Nama Saya adalah $name, usia Saya saat ini adalah $age tahun.');
 }
 //2. Type Inference
-void TypeInference(var name, var age){
+void typeInference(var name, var age){
   print('Nama Saya adalah $name, usia Saya saat ini adalah $age tahun.');
 }
 //3. String Interpolation
-void StringInterpolation(var name, int age){
+void stringInterpolation(var name, int age){
   print('Nama Saya adalah $name, usia Saya saat ini adalah $age tahun.');
 }
 //4. Multi-paradigm: OOP & Functional
