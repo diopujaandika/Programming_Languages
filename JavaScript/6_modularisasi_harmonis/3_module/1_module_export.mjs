1278b4a5d31234a9bf67e89080526f1cf8479e82
diopujaandika <dpujaandika17@gmail.com>
2025-10-25 23:52:03 +0700

    update module export in js

--- JavaScript/6_modularisasi_harmonis/3_module/1_module_export.mjs
+++ JavaScript/6_modularisasi_harmonis/3_module/1_module_export.mjs
@@ -4,20 +4,18 @@
  */
 
 //Cara Export Variable
-    export const name = 'Dicoding'
-    export const favoriteFood = ['Pizza', 'Pasta', 'Shushi']
+    export const name = 'Dicoding';
+    export const favoriteFood = ['Pizza', 'Pasta', 'Shushi'];
 
 //Cara Export Function
     export function sayHi(name){
-        console.log(`Hi, ${name}`)
+        console.log(`Hi, ${name}`);
     }
 
 //Sebenarnya tidak perlu menuliskan kata kunci export disetiap nilai yang ingin dieksport. Kita cukup mengeksport diakhir berikas:
-    const nama = 'Dicoding'
-    const makananKesukaan = ['Mi Ayam', 'Sate', 'Nasi Goreng', 'Bakso']
-
+    const nama = 'Dicoding';
+    const makananKesukaan = ['Mi Ayam', 'Sate', 'Nasi Goreng', 'Bakso'];
     function iniSaya(nama, makananKesukaan){
-        console.log(`Hai, ini ${nama}, makanan kesukaan saya adalah ${makananKesukaan}`)
+        console.log(`Hai, ini ${nama}, makanan kesukaan saya adalah ${makananKesukaan}`);
     }
-
-    export{nama, makananKesukaan, iniSaya}
+    export{nama, makananKesukaan, iniSaya};
