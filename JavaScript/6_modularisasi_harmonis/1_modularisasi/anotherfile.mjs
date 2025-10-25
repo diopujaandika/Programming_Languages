841da2452108c98ea43d06acaaafff3abdcfde9a
diopujaandika <dpujaandika17@gmail.com>
2025-10-25 20:11:12 +0700

    udpate make import module in js

--- JavaScript/6_modularisasi_harmonis/1_modularisasi/anotherfile.mjs
+++ JavaScript/6_modularisasi_harmonis/1_modularisasi/anotherfile.mjs
@@ -1,3 +1,9 @@
 export default function myFunction(){
     console.log('Ini adalah function export default.');
 }
+
+const name = 'Marisa Amellia';
+const email = 'marisaamellia.com';
+const age = 25;
+
+export{name, email, age}
--- JavaScript/6_modularisasi_harmonis/1_modularisasi/main.mjs
+++ JavaScript/6_modularisasi_harmonis/1_modularisasi/main.mjs
@@ -1,3 +1,22 @@
 import myFunction from "./anotherfile.mjs";
-
 myFunction();
+
+import result from "./anotherfile.mjs";
+result();
+
+import {name, email, age} from './anotherfile.mjs';
+console.log(name);
+console.log(email);
+console.log(age);
+
+import * as variable from './anotherfile.mjs';
+console.log(variable.name);
+console.log(variable.email);
+console.log(variable.age);
+
+import { myFunction as UserFunction } from "./user.mjs";
+import { myFunction as customerFunction } from "./customerFunction.mjs";
+
+UserFunction();
+customerFunction();
+
