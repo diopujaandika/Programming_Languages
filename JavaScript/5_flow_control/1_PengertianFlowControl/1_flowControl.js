48f7459556b76614c40a211663da7d048f20e665
diopujaandika <dpujaandika17@gmail.com>
2025-10-25 07:32:44 +0700

    update if and switch statement in js

--- JavaScript/5_flow_control/1_PengertianFlowControl/1_flowControl.js
+++ JavaScript/5_flow_control/1_PengertianFlowControl/1_flowControl.js
@@ -5,5 +5,35 @@
  * Jenis-jenis Flow Controls
  * 1. Conditional flow adalah cara untuk menentukan apakah code dieksekusi atau dilewatkan.
  *      a. If Statement
- *      b. 
+ *      b. Switch Case
  */
+//If Else Statement
+const studentScore = (score) => {
+    if (score > 90){
+        console.log('Selamat, Anda mendapatkan nilai A!');
+    } else if (score > 80){
+        console.log('Selamat, Anda lulus ujian!');
+    }else{
+        console.log('Maaf, Anda belum lulus ujian!');
+    }
+}
+studentScore(87);
+
+// Switch Statement adalah control flow statement yang mengevaluasi expression terhadapap beberap kasus.
+
+const nameFruit = (fruit) => {
+    switch (fruit) {
+        case 'banana':
+            console.log('I am a Banana!');
+            break;
+        case 'apple':
+            console.log('I am a Apple!');
+            break;
+        case 'orange':
+            console.log('I am a Orange!');
+            break;
+        default:
+            console.log(`I am not a fruit. I am a ${fruit}!`);
+    }
+}
+nameFruit('web developer');
