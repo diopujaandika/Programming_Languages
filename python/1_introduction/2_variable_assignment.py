2bdcbaa24ba46dbd189633062daaabe204118835
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 12:47:39 +0700

    variable dan assignment

--- python/1_introduction/2_variable_assignment.py
+++ python/1_introduction/2_variable_assignment.py
@@ -1,3 +1,20 @@
+#VARIABLE
+#Variable merujuk kepada lokasi dalam komputer yang digunakan untuk menyimpan nilai dengan tipe data tertentu.
+
+greeting = 'Hello World!' #Ini adalah assignment
+print(greeting)
+"""
+Output: Hello World!
+"""
+
+
+"""
+Addition adalah variabel yang bernilai ekspresi 2+2,
+result adalah hasil pengurangan dari variable additon dikurangi 1
+"""
 addition = 2 + 2
 result = addition - 1
 print(result)
+"""
+Ouput: 3
+"""
