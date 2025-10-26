b91b8698cacd18a121fa883ffb2e4db1c449fd63
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 11:13:35 +0700

    update introduction in dart

---
+++ Dart/bin/1_introduction/3_dart_platform.dart
@@ -0,0 +1,6 @@
+/**
+ * DART NATIVE (VM JIT & AOT)
+ *    Ditujukan untuk program yang menargetkan perangkat seluler, desktop, server, dan lainnya. Dart native mencakup Dart VM dengan kompilasi JIT (Just In Time) dan Kompiler AOT (Ahead Of Time) untuk menghasilkan kode mesin.
+ * DART WEB (JAVASCRIPT)
+ * Dart Web menyertakan kompiler development(dartdevc) dan kompiler production (dart2js).
+ */
