3b9ec56edd30f8fc77aa24d4b41925026c6da977
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 09:03:39 +0700

    hello js

--- JavaScript/1_introduction/1_javaScript.js
+++ JavaScript/1_introduction/1_javaScript.js
@@ -1,10 +1,42 @@
-//Pemrograman JavaScript
+//JavaScript
 /**
- * Pemrograman adalah sekumpulan instruksi sistematis yang akan dieksekusi dengan komputer. 
+ * Pemrograman adalah proses menyusun instruksi yang sistematis dam akan dieksekusi oleh komputer. Untuk membuat instruksi yang dapat dimengerti oleh komputer kita membutuhkan bahasa pemrograman, dan salah satunya adalah JavaScript atau JS.
+ * JavaScript atau JS adalah salah satu bahasa pemrograman yang paling populer untuk membuat sebuah aplikasi berbasis website.
+ * Selain berbasis web, saat ini JS sudah dapat digunakan untuk membuat aplikasi berbasis cellphone, dekstop, command line, hingga model dengan machine learning.
  */
-var name = "Marisa Amelia";
-var age = 22;
+var greeting = "Hello World!";
+var name = "Dio Puja Andika";
+console.log(greeting);
+console.log(name)
+console.log(`Welcome to the ${greeting}. Nice to meet you Mr. ${name}`);
 
-console.log(name);
-console.log(age);
-console.log(`Hai, nama saya adalah ${name}, saya berusia ${age}`);
+/**
+ * Serangkaian perintah komputer ditulis oleh developer dalam bentuk kode. Kode dapat berupa sekumpulan:
+ * 1. Karakter
+ * 2. Angka
+ * 3. Simbol
+ * Kode kode yang ditulis oleh developer dapat dikategorikan kedalam bahasa tingkat tinggi (High Level Language), karena kode yang ditulis masih dapat dimengerti oleh manusia.
+ * Hasil kegitan menulis kode ini disebut dengan "Source Code". 
+ * Source code akan diubah ke bahasa yang dapat dipahami oleh komputer/mesin sehingga bisa dianalisis dan dieksekusi perintah-perintahnya. Ini lah yang disebut dengan bahasa tingkat rendah (Low Level Language) karena kode hanya dapat dipahami oleh mesin.
+ */
+
+/**
+ * Terdapat dua tahapan perubahan bahasa tingkat tinggi ke bahasa tingkat rendah, yaitu dengan Compiler dan Interpreter:
+ * 
+ * 1. COMPILED LANGUAGE
+ * Compiled berarti bahasa pemrograman yang ditulis harus kompilasi sebelum dijalankan oleh mesin.
+ * Kode => Kompilasi => Machhine
+ * Contoh: Java, C++. Swift, dsb.
+ * 
+ * 2. SCRIPTING LANGUAGE
+ * Scripting berarti bahasa pemrograman akan langsung diterjemahkan oleh mesin tanpa memerlukan compiler.
+ * Kode => Machie
+ * Contoh: JavaScript, Python, PHP, dsb.
+ */
+
+/**
+ * JS pada awalnya hanya sebatas untuk membuat web browser menjadi lebih interaktif dan dinamis, tapi saat ini JS sudah digunakan pada setiap platform. Lingkungan pengembangan JS itu:
+ * 1. Browser
+ * 2. Node.js
+ * 3. Bun
+ */
