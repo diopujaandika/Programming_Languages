60bec39bd7539b6d4361f0185f0338cd0a384593
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 12:56:55 +0700

    update input, output, & comment in python

--- python/1_introduction/3_input_output_comment.py
+++ python/1_introduction/3_input_output_comment.py
@@ -1,15 +1,28 @@
-#Inline Comment
+#INPUT => memungkinkan pengguna untuk memberikan masukan, Sintak: input()
+name = input('Masukkan nama Anda: ')    #Input()
+"""
+Output:
+Masukkan nama Anda: Perseus Evans
+"""
 
-name = input('Masukkan nama Anda: ') #Input()
-
-#Output
+#Output => menampilkan output ke layar komputer, Sintak: print(name)
 print(name) #Output()
+"""
+Output:
+Perseus Evans
+"""
 
+#Comment atau komentar merupakan barisan teks yang akan diabaikan oleh python ketika program dijalankan.
+
+#Inline Commnet => Komentar satu baris. Sintak #Comment
+#Variable ini menyimpan nama 'Perseus Evans'
+name = 'Perseus Evans'
+
+#Block Commnet => Satu block kode dengan tujuan menjelaskan kode komplleks atau membuat dokumentasi dari sebuah fungsi atau modul. Sintak: """Comment""" atau '''Comment'''
 """
 Ini adalah Block Comment,
 Teks ini akan diabaikan oleh Python.
 """
-
 '''
 Ini adalah Block Comment,
 Teks ini akan diabaikan oleh Python,
