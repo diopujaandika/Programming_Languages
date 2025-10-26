5a3926c0f2a6263bbf5b385340d2f8fd3df13138
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 16:26:30 +0700

    update learn about a kotlin

--- kotlin/kotlin_basic1/src/main/kotlin/fundamental/error_bug.kt
+++ kotlin/kotlin_basic1/src/main/kotlin/fundamental/error_bug.kt
@@ -1,6 +1,19 @@
 package com.diopujaandika.kotlin.fundamental
 
+//Error dan bug adalah istilah yang digunakan untuk menadakan suatu kesalahan yang terjadi pada program.
 fun main(){
     println("Error")
+    println("Error adalah kesalahan teknis yang tidak dapat ditangani sistem sehingga program gagal untuk dijalankan.")
+    /* Ciri-ciri Error
+    * 1. Program tidak bisa dijalankan
+    * 2. Terdapat pesan error
+    * 3. Biasanya disebabkan oleh kesalahan teknis
+    * */
     println("Bug")
+    println("Bug adalah sebuah istilah umum yang digunakan untuk menandakan kesalahan yang sifatnya teknis atau non teknis, dan biasanya bug belum tentu dapat dikatakan error. ")
+    /* Ciri-ciri Bug
+     * 1. Program tetap berjalan
+     * 2. Output atau perilaku program tidak sesuai dengan ekspetasi
+     * 3. Sering muncul akibat kesalahan logika, asumsi yang keliru, atau perhitungan yang salah.
+     */
 }
