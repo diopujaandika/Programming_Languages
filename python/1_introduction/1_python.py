1fccb325c83615708d0f925c7f3bc4b31814b023
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 12:14:42 +0700

    update introduction in python

---
+++ python/1_introduction/1_python.py
@@ -0,0 +1,15 @@
+#PYTHON 
+"""
+Python adalah bahasa pemrograman multifungsi.
+Rilis: 1991
+Pencipta: Guido van Rossum (GvR)
+Python itu mudah dibaca dan dimengerti (readable)
+Python itu memiliki kemampuan penanganan kesalah (exception handling)
+Python dirancang untuk diimplemetasikan di:
+1. Website(Server-Side)
+2. Analisis Data
+3. Machine Learning
+"""
+
+print("Hello World!")
+
