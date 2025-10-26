636b038d964882a5d1278001e9909aabdc3f0908
diopujaandika <dpujaandika17@gmail.com>
2025-10-26 09:25:56 +0700

    history js

---
+++ JavaScript/1_introduction/2_historyjs.js
@@ -0,0 +1,13 @@
+/**
+ * SEJARAH JAVASCRIPT
+ * JavaScript atau JS dilahirkan sekitar tahun 1995 oleh progremmer dari perusahaan Netscape, yaitu Brendan Eich dengan tujuan membuat website dapat lebih interaktif dan dinamis.
+ * Pada awalnya JavaScript memiliki nama LiveScirpt, akan tetapi karena pada saat itu Java merupakan bahasa pemrograman yang sangat populer akhirnya LiveScript diubah menjadi JavaScipt dengan tujuan JS dapat mengikuti kepopuleran dari Java. Walapun kelihatannya JS dan Java terdengar sama, tapi mereka berdua tidak ada relasi sama sekali.
+ */
+
+/**
+ * Kemudian setelah di adobsi di luar Netscape, Js distandarisasi oleh organisasi Internasional bernama European Computer Manufacturers Association (ECMA), dan standar bahasa pemrogaman di bawah lisensi ECMA akan mengikuti standart dari ECMA, yang dikenal dengan ECMAScript.
+ * Sehingga JavaScript akan selalau mengikuti standar dari ECMAScript.
+ * Jadi:
+ * 1. JavaScript itu => Bahasa Pemrograman
+ * 2. ECMAScript itu => Standar Bahasa Pemrograman
+ */
