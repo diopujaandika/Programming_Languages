7d6e6aedf9148de7de29285e405cad75b3c89406
diopujaandika <dpujaandika17@gmail.com>
2025-11-01 21:46:04 +0700

    update opp in js

--- JavaScript/7_menyelamiDuniaOOP/0_oop/1_constructor_function.js
+++ JavaScript/7_menyelamiDuniaOOP/0_oop/1_constructor_function.js
@@ -1,19 +1,25 @@
-//CONSTRUCTOR FUNCTION
+// CONSTRUCTOR FUNCTION
+/*
+    Js bukan bahasa pemrograman berbasis class, sehingga JS tidak mengenal konsep class.
+    Untuk mengcounter hal ini diciptakan yang namanya costructtor function.
+    Constructor function adalah cara yang digunakan untuk membuat object dan class sebelum adanya ES6.
+*/
 
-function Person(name, age){
-    this.name = name
-    this.age = age
+function Person(name, age) {
+    this.name = name; //Properti
+    this.age = age; //Properti
 }
 
-Person.prototype.eat = function(){
-    console.log(`${this.name} is eating!`)
+//Method
+Person.prototype.eat = function() { //Menggunakan prototype
+    console.log(`${this.name} is eating!`);
 }
 
-const person1 = new Person('Alice', 30)
-const person2 = new Person('Bob', 34)
+const person1 = new Person('Alice', 30);
+const person2 = new Person('Bob', 25);
 
-console.log(person1.name)
-console.log(person2.name)
+console.log(person1.name);
+console.log(person2.name);
 
-person1.eat()
-person2.eat()
+person1.eat();
+person2.eat();
--- JavaScript/7_menyelamiDuniaOOP/0_oop/2_es6_class.js
+++ JavaScript/7_menyelamiDuniaOOP/0_oop/2_es6_class.js
@@ -1,25 +1,30 @@
-//ES6 CLASS
+// ES6
+/*
+    ES6 Class
+    
+ */
 
-//Membuat class
-class Person{
-    //Membuat cunstructor class
-    constructor(name, age){
-        this.name = name
-        this.age = age
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
 
-    //Membuat Method
-    eat(){
-        console.log(`${this.name} is eating!`)
+    eat() {
+        console.log(`${this.name} is eating.`);
     }
 }
 
-//Membuat instance dari Person
-const person1 = new Person('Alice', 30)
-const person2 = new Person ('Bob', 34)
+// Membuat instance dari Person
+const person1 = new Person('Alice', 30);
+const person2 = new Person('Bob', 25);
 
-console.log(person1.name) //Output: Alice
-console.log(person2.name) //Output: Bob
+console.log(person1.name);  // Output: Alice
+console.log(person2.name);  // Output: Bob
 
-person1.eat() //Output: Alice is eating
-person2.eat() //Output: Bob is eating
+person1.eat();
+person2.eat();
+
+// Tetap function dan bukan class
+console.log(typeof Person);
+
