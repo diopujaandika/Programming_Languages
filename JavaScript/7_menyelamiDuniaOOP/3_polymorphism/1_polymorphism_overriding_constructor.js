68b8b56f04f69824d00a79c958e35a37c9e158fb
diopujaandika <dpujaandika17@gmail.com>
2025-11-02 13:16:00 +0700

    update overriding constructor

--- JavaScript/7_menyelamiDuniaOOP/3_polymorphism/1_polymorphism_overriding_constructor.js
+++ JavaScript/7_menyelamiDuniaOOP/3_polymorphism/1_polymorphism_overriding_constructor.js
@@ -1,24 +1,32 @@
-/**
- * POLYMORPHISM
- * Polymorphism 
- */
+/*
+    POLYMORPHISM
+    Polymorphism adalah pilar OOP yang memungkinkan SubClass mengubah property atau method yang diturunkan/diwariskan oleh SuperClass.
+    Untuk mengubah implementasi method kita dapat menggunakan konsep yang disebut dengan overriding.
+*/
+/*
+    Overriding adalah cara untuk membuat implementasi yang berbeda di SubClass untuk menthod yang diturunkan dari SuperClass.
+    Overriding dapat diterapkan untuk membuat method yang lebih spesifik di SubClass.
+    Overridng juga dapat diterapkan untuk menambahkan property baru SubClass.
+    Overriding dapat diterapkan pada constructor maupun pada method.
+*/
 
-class SmartPhones{
-    constructor(color, brand, model){
-        this.color = color
-        this.brand = brand
-        this.model = model
+// Overridng Constructor
+class SmartPhones {
+    constructor(color, brand, model) {
+        this.color = color;
+        this.brand = brand;
+        this.model = model;
     }
     charging(){
-        console.log('Charging is', this.model)
+        console.log('Charging is', this.model);
     }
 }
 
 class Android extends SmartPhones{
-    //OVerriding Constructor
+    // Overriding Constructor
     constructor(color, brand, model, device){
-        super(color, brand, model)
-        this.device = device
+        super(color, brand, model);
+        this.device = device;
     }
     splitScreen(){
         console.log('Android have a Split Screen')
