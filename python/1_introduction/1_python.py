02eef86b1e48bf255ca0327253203949ff73544f
Dio Puja Andika <dpujaandika17@gmail.com>
2026-04-13 08:54:05 +0700

    update

--- python/1_introduction/1_python.py
+++ python/1_introduction/1_python.py
@@ -7,7 +7,7 @@ Python itu mudah dibaca dan dimengerti (readable)
 Python itu memiliki kemampuan penanganan kesalah (exception handling)
 Python dirancang untuk diimplemetasikan di:
 1. Website(Server-Side)
-2. Analisis Data
+2. Analisis Data  
 3. Machine Learning
 """
 
