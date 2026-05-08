a6bf893c4992acb42cf654f126df8c21ca4ced77
Dio Puja Andika <dpujaandika17@gmail.com>
2026-05-08 23:17:09 +0700

    udpate

---
+++ JavaScript/9_asynchronous_process/asynchronous.js
@@ -0,0 +1,4 @@
+// ASYNCHRONOUS PROCESS
+/*
+ * Asynchronous adalah sebuah operasi yang memungkinkan dijalankan oleh mesin dan kemudian dapat beralih fokus untuk menjalankan tugas-tugas (operasi) berikutnya sembari menunggu operasi sebelumnya selesai.
+ */
