63053b28670c9489b43d476822fe5354ac0d2031
Dio Puja Andika <dpujaandika17@gmail.com>
2026-09-23 04:25:28 +0700

    update | data type

--- JavaScript/2_fundamental_syntac/7_mengubahTipeData.js
+++ JavaScript/2_fundamental_syntac/7_mengubahTipeData.js
@@ -6,48 +6,48 @@
  */
 
 //1. Konversi Eksplisit
-    //a. Mengubah ke String dengan fungsi String(variable) dan variable.toString()
-        const number = 123
-        const boolean = true
+    //a. Mengubah ke String dengan fungsi String(NamaVariable); dan namaVariable.toString();
+        const number = 123;
+        const boolean = true;
         //Cara Convert
-        const strNumber = String(number) //String(variable)
-        const strBoolean = boolean.toString() //variable.toString()
+        const strNumber = String(number); //String(variable);
+        const strBoolean = boolean.toString(); //variable.toString();
         //Output
         console.log(strNumber, strBoolean);
 
-    //b. Mengubah ke Number dengan Number(variable)
-        const strNumber2 = '123'
-        const strFloat2 = "3.14"
+    //b. Mengubah ke Number dengan Number(namaVariable);
+        const strNumber2 = '123';
+        const strFloat2 = "3.14";
         //Convert
-        const numFormString = Number(strNumber2)
-        const floatFromString = Number(strFloat2)
-        const numFromBoolean = Number(boolean)
+        const numFormString = Number(strNumber2);
+        const floatFromString = Number(strFloat2);
+        const numFromBoolean = Number(boolean);
         //Output
-        console.log(numFormString, floatFromString, numFromBoolean)
+        console.log(numFormString, floatFromString, numFromBoolean);
 
-        const cm = '20cm'
-        const px = '61.1px'
+        const cm = '20cm';
+        const px = '61.1px';
         //Convert
-        const intFromCM = parseInt(cm)  //parseInt(variable) untuk menjadi bilangan bulat
-        const intFromPX = parseFloat(px) //parseFloat(variable) untuk menjadi bilangan pecahan
+        const intFromCM = parseInt(cm);  //parseInt(namaVariable); untuk menjadi bilangan bulat
+        const intFromPX = parseFloat(px); //parseFloat(namaVariable); untuk menjadi bilangan pecahan
         //Output
-        console.log(intFromCM, intFromPX)
+        console.log(intFromCM, intFromPX);
 
-    //c. Mengubah ke Boolean dengan fungsi Boolean(variable)
-        const boolNumber = 123
-        const boolString = 'Dicoding'
-        const boolEmpty = null
+    //c. Mengubah ke Boolean dengan fungsi Boolean(namaVariable);
+        const boolNumber = 123;
+        const boolString = 'Dicoding';
+        const boolEmpty = null;
         //Convert
-        const boolFormNumber = Boolean(boolNumber)
-        const boolFromString = Boolean(boolString)
-        const boolFromNull = Boolean(boolEmpty)
+        const boolFormNumber = Boolean(boolNumber);
+        const boolFromString = Boolean(boolString);
+        const boolFromNull = Boolean(boolEmpty);
         //Output
-        console.log(boolFormNumber, boolFromString, boolFromNull)
+        console.log(boolFormNumber, boolFromString, boolFromNull);
 
 //2. Konversi Implisit itu bergantung keadaan operator yang digunakan
-    const myAge = 23
-    const myMessage = 'Usiaku: ' + myAge
-    console.log(myMessage, typeof(myMessage))
-    const myBool = true
-    const myResult = 14 + myBool
-    console.log(myResult)
+    const myAge = 23;
+    const myMessage = 'Usiaku: ' + myAge;
+    console.log(myMessage, typeof(myMessage));
+    const myBool = true;
+    const myResult = 14 + myBool;
+    console.log(myResult);
