c2a2a9573b015b93f971f92aa3f79ef48943e738
Dio Puja Andika <dpujaandika17@gmail.com>
2026-09-23 04:42:38 +0700

    update

--- JavaScript/2_fundamental_syntac/15_TODO.js
+++ JavaScript/2_fundamental_syntac/15_TODO.js
@@ -1,3 +1,4 @@
+//LATIHAN 1
 /**
  * TODO:
  * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
@@ -13,3 +14,20 @@ const age = 24;
 const isMarried = false;
 
 console.log(firstName, lastName, age, isMarried);
+
+//LATIHAN 2
+// TODO: buatlah variabel (konstan) bernama `currency` dan isi dengan nilai "IDR".
+const currency = "IDR";
+
+// TODO: buatlah variabel bernama `value` dan isi dengan nilai 10000.
+let value = 10000;
+
+// TODO: tambahkan nilai di dalam variabel `value` sebesar 5000.
+value += 5000;
+
+/**
+ * TODO: buatlah variabel (konstan) bernama `money`,
+ * isi dengan penambahan string dari nilai `currency` + " " + `value`.
+ */
+const money = currency + " " + value;
+console.log(money);
