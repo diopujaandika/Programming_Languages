4358bbd523a4dfdff857cc68e48826734ce1d2ab
Dio Puja Andika <dpujaandika17@gmail.com>
2026-09-23 03:42:52 +0700

    update

--- JavaScript/2_fundamental_syntac/1_statement.js
+++ JavaScript/2_fundamental_syntac/1_statement.js
@@ -1,23 +1,16 @@
 /**
  * Statement adalah instruksi yang akan dieksekusi oleh komputer.
+ * Jadi dalam pemrograman suatu instruksi itu disebut dengan statement.
+ * In programming an that instruction 
+ * Usually in JS, to end a statement we put a semicolon (;) at the end of the statement.
  */
 
-//Contoh 1
-const age = 10 //Statement 1
-const name = 'dicoding' //Statement 2
-console.log(`Aku ${name}, umurku ${age} tahun.`) //Statement 3
+//Contoh 1 : Membuat statement dengan 2 buah variable yaitu, age dan name yang diberi value 10 dan dicoding
+const age = 10; //Statement 1
+const name = 'dicoding'; //Statement 2
+console.log(`Aku ${name}, umurku ${age} tahun.`); //Statement 3
 
-//Contoh 2
-const nama = 'Dio Puja Andika' //Statement 1
-const usia = 22 //Statement 2
-console.log(`Aku ${nama}, angka favoriteku ${usia} tahun.`) //Statement 3
-
-//Contoh 3
-const namaku = 'Deterra D. Puja Al-yasinta' //Statement 1
-const umurku = 17 //Statement 2
-console.log(`Hai, namaku ${namaku}, umurku saat ini adalah ${umurku}`) //Statement 3
-
-//Contoh 4 : Dalam JS kita bisa membuat beberapa statement dalam satu line dengan memanfaatkan tanda (;) sebagai tanda berakhirnya statement.
+//Contoh 2 : Dalam JS kita bisa membuat beberapa statement dalam satu line dengan memanfaatkan tanda (;) sebagai tanda berakhirnya statement.
 const statement1 = "Ini Statement 1"; const statement2 = "Ini statement 2"; const statement3 = "Ini Statement 3"; console.log(statement1, statement2, statement3);
 
 //Contoh 5: 
--- JavaScript/2_fundamental_syntac/5_variabel.js
+++ JavaScript/2_fundamental_syntac/5_variabel.js
@@ -1,12 +1,15 @@
 /**
  * VARIABLE
- * Variable adalah wadah untuk menampung sebuah nilai
+ * Variable adalah wadah untuk menampung sebuah nilai.
+ * Value yang ditampung bisa berupa angka, teks, ataupun yang menghasilkan value (expression).
  * 
  * 4 Cara membuat varible pada JS
  *      1. automatically => Otomatis akan mengetahui nilainya
  *      2. var => keyword variable pada js yang tidak bisa diinisialisasi ulang dan boleh dikosongkan.
  *      3. let => masih bisa diinisialisasi ulang nilainya dan boleh dikosongkan
- *      4. const => keyword variable pada js yang tidak bisa diinisialisasi ulang nilainya dan tidak boleh dikosongkan
+ *      4. const => keyword variable pada js yang tidak bisa diinisialisasi ulang nilainya dan tidak boleh dikosongkan.
+ * 
+ * NOTE: Umumnya pembuatan variable dalam JS menggunakan sintaksis const dan let
  */
 
 //Contoh automatically
@@ -29,10 +32,14 @@
 
 //Contohh 1
 const id = 123;
-let username = 'Dicoding';
+let username = 'Dicoding'
 
-console.log(id); //Output : 123
-console.log(username); //Output : Dicoding
+console.log(id); //Output: 123
+console.log(username); //Output: Dicoding
+
+console.log('Sebelum diubah', username); //Output: Sebelum diubah: Dicoding
+username = 'Dicoding Academy';
+console.log('Setelah diubah', username); //Output: Setelah diubah: Dicoding Academy
 
 //ATURAN PENAMAAN VARIABLE
 //1. Tidak boleh memberikan nama yang sama dalam cakupan yang sama
@@ -44,4 +51,24 @@ console.log(username); //Output : Dicoding
     // const name = 'Jhon' //SyntacError karena kita menggunakan nama variable yang sama pada cakupan yang sama
     const division = 'IT'
     
-//2. 
+//2. Nama variable hanya terdiri darai karakter tertentu
+    //Nama variabel yang benar
+    const firstName = 'Fulan';
+    const last_name = 'Lestari';
+    const $message = 'Hello, World!';
+    const userId1 = 123;
+    const userId2 = 456;
+
+    //Nama Variabel yang salah
+    // const first-Name = 'Fulan'; //Tidak boleh mengandung karakter -
+    // const last name = 'Lestari'; //Tidak boleh mengandung spasi
+    // const @message = 'Hello, World!'; //Tidak boleh mengandung karakter @
+
+//3. Nama variable tidak boleh diawali dengan angka
+    //Nama variable yang benar
+    const firstNameAgain = 'Fulan';
+    const _secondName = 'Fulana';
+    
+    //Nama variable yang salah karena diawali dengan angka
+    // const 1stName = 'Fulan';
+    // const 2ndName = 'Fulana';
